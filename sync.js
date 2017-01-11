3855f28c350c8476f562a791740d689b1a01c556
Ludimila <lbelacruz@gmail.com>
2017-01-11 18:06:07 -0200

    Exercicio 3

--- sync.js
+++ sync.js
@@ -1,5 +1,5 @@
-var fs = require('fs')
-var contents = fs.readFileSync(process.argv[2])
-var lines = contents.toString().split('\n').length - 1
+var fs = require('fs') ////require lets the machine know to use a specific module for your code to work
+var file = fs.readFileSync(process.argv[2]) //readFileSync() returns the contents of a file as a buffer/array object
+var lines = file.toString().split('\n').length - 1 //toString converts an array/buffer into a string, split() splits a string into an array, splitting up objects based on the contents of the brackets ()
 
 console.log(lines)

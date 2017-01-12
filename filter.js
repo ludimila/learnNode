57ce301ca77d20f5614860c29c44df4e091e4d2e
Ludimila <lbelacruz@gmail.com>
2017-01-12 16:23:01 -0200

    exercicio 5

--- filter.js
+++ filter.js
@@ -3,13 +3,15 @@
 
 var fs = require('fs')
 var file = process.argv[2]
+var endFile = process.argv[3]
+
 
 fs.readdir(file, function (err, contents) {
 	if (err){
 		console.log("Deu Bad")
 	}else{
 		 contents.filter(function (item){ 
-             return item.indexOf('.' + process.argv[3]) >= 0 //procura . e a extensão no fim do array
+             return item.indexOf('.' + endFile) >= 0 //procura . e a extensão no fim do array
          }).forEach(function (item) {
              console.log(item);
          })
